12100a6b99aa9d379baa9903adc7bc17601aac94
yashthedev22 <yashjoshi15510@gmail.com>
2026-06-18 22:42:03 +0530

    added new parameters

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Request
 #from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from fastapi.staticfiles import StaticFiles
+from fastapi import HTTPException, status
 
 
 
@@ -30,7 +31,8 @@ app.mount("/static",StaticFiles(directory="static"),name="static")
 templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/",include_in_schema=False ) #it will hide it from documantation
+@app.get("/",include_in_schema=False, name = "home" ) #it will hide it from documantation
+@app.get("/post",include_in_schema=False, name = "post")
 def home(request : Request):
     return templates.TemplateResponse(request, "home.html" , {"posts":posts})
 
@@ -42,3 +44,14 @@ def holi():
 @app.get("/api/post")
 def get_posts():
     return posts
+
+
+@app.get("/api/post/{post_id}")
+def get_post(post_id: int):
+    for post in posts:
+        if post.get("id") == post_id:
+            return post
+    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="post not found")
+
+
+   
