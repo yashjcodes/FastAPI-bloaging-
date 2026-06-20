211a32d56406ad79744b575f768a52db5a680993
yashthedev22 <yashjoshi15510@gmail.com>
2026-06-20 23:48:38 +0530

    added code for sqlalchmey

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from fastapi.staticfiles import StaticFiles
 from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 from starlette.exceptions import HTTPException as StarletteHTTPException
-
+from schemas import PostCreate , PostResponse 
 
 posts :list[dict] = [
        {
@@ -49,12 +49,31 @@ def post_page(request: Request, post_id: int):
         
 
 
-@app.get("/api/post")
+@app.get("/api/post" , response_model = list[PostResponse])
 def get_posts():
     return posts
 
 
-@app.get("/api/post/{post_id}")
+
+@app.post(
+    "/api/posts",
+    response_model=PostResponse,
+    status_code=status.HTTP_201_CREATED,
+)
+def create_post(post: PostCreate):
+    new_id = max(p["id"] for p in posts) + 1 if posts else 1
+    new_post = {
+        "id": new_id,
+        "author": post.author,
+        "title": post.title,
+        "content": post.content,
+        "date_posted": "April 23, 2025",
+    }
+    posts.append(new_post)
+    return new_post
+
+
+@app.get("/api/post/{post_id}", response_model = PostResponse)
 def get_post(post_id: int):
     for post in posts:
         if post.get("id") == post_id:
